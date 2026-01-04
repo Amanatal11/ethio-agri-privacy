4951e67ae29a8ac4325b73b080385abd9b5c5e9b
Amanatal11 <aman.atalay-ug@aau.edu.et>
2026-01-04 23:18:00 +0300

    fixed import message

--- benchmarks/privacy_eval.py
+++ benchmarks/privacy_eval.py
@@ -1,5 +1,5 @@
 import numpy as np
-from tools.privacy_engine import PrivacyEngine
+from ethio_agri_advisor.tools.privacy_engine import PrivacyEngine
 from typing import List, Dict
 
 class PrivacyEvaluator:
